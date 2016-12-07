c15dce30f9fdfb523c80ee648ed87a8afb49344a
peterfei <peterfeispace@gmail.com>
2016-12-07 14:28:59 +0800

    Initial Commit

---
+++ ruby-china-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ruby-china-mac
+//
+//  Created by peterfei on 2016/12/7.
+//  Copyright © 2016年 peterfei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
